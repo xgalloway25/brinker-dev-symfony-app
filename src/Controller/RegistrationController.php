7f8eeb00e2c40cf55ea6fc698e9ba71ff6150a9e
Xavier Galloway <xaviergalloway@Xaviers-MBP.attlocal.net>
2024-05-24 16:31:23 -0500

    create authentication controllers

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Guest;
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+
+class RegistrationController extends AbstractController
+{
+    #[Route('/register', name: 'app_register')]
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $guest = new Guest();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+            $guest->setFirstName("New")
+                ->setLastName("User")
+                ->setEmail($user->getEmail());
+            $user->setGuestId($guest);
+            $entityManager->persist($user);
+            $entityManager->persist($guest);
+            $entityManager->flush();
+
+            // do anything else you need here, like send an email
+            // return $this->redirectToRoute('app_guest_show', ['id' => $guest->getId()]);
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('registration/register.html.twig', [
+            'registrationForm' => $form,
+        ]);
+    }
+}
